815d662ce0eb32dba2b5ea90224bef6d8204facd
Maheshnmg <35828973+Maheshnmg@users.noreply.github.com>
2020-04-18 03:24:34 +0530

    Added comment

--- Vue/src/com/util/Util.java
+++ Vue/src/com/util/Util.java
@@ -6,6 +6,9 @@ import com.dto.TestCase;
 
 public class Util {
 
+	/**
+	 * Method getTCObjByMatchId()
+	 */
 	public static TestCase getTCObjByMatchId(List<TestCase> tcList, int tcId) {
 		return (tcList != null && !tcList.isEmpty()) ? (TestCase) tcList.stream()
 															 			.filter(p -> p.getTestCaseID() == tcId)
